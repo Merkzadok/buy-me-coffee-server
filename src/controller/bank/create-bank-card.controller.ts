e1788d0e0dc030ba82b0641495541c952b131678
25LP1749 25LP1749 <merkzadok@gmail.com>
2025-08-04 12:47:52 +0800

    check

--- src/controller/bank/create-bank-card.controller.ts
+++ src/controller/bank/create-bank-card.controller.ts
@@ -1,44 +1,44 @@
 import { Response, Request } from "express";
 import { prisma } from "../../utils/prisma";
-// function isValidCardNumber(cardNumber: string): boolean {
-//   if (!/^\d{16}$/.test(cardNumber)) return false;
+function isValidCardNumber(cardNumber: string): boolean {
+  if (!/^\d{16}$/.test(cardNumber)) return false;
 
-//   let sum = 0;
-//   let shouldDouble = false;
+  let sum = 0;
+  let shouldDouble = false;
 
-//   for (let i = cardNumber.length - 1; i >= 0; i--) {
-//     let digit = parseInt(cardNumber[i], 10);
+  for (let i = cardNumber.length - 1; i >= 0; i--) {
+    let digit = parseInt(cardNumber[i], 10);
 
-//     if (shouldDouble) {
-//       digit *= 2;
-//       if (digit > 9) digit -= 9;
-//     }
+    if (shouldDouble) {
+      digit *= 2;
+      if (digit > 9) digit -= 9;
+    }
 
-//     sum += digit;
-//     shouldDouble = !shouldDouble;
-//   }
+    sum += digit;
+    shouldDouble = !shouldDouble;
+  }
 
-//   return sum % 10 === 0;
-// }
+  return sum % 10 === 0;
+}
 
-// function isValidExpiryDate(dateStr: string): boolean {
-//   const expiryDateRegex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
-//   if (!expiryDateRegex.test(dateStr)) return false;
+function isValidExpiryDate(dateStr: string): boolean {
+  const expiryDateRegex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
+  if (!expiryDateRegex.test(dateStr)) return false;
 
-//   const expiryDate = new Date(dateStr);
-//   const today = new Date();
+  const expiryDate = new Date(dateStr);
+  const today = new Date();
 
-//   return expiryDate > today;
-// }
+  return expiryDate > today;
+}
 
 export const createBankCard = async (req: Request, res: Response) => {
   const { country, firstName, lastName, cardNumber, expiryDate } = req.body;
 
   const { userId } = req.params;
 
-  //   if (!isValidExpiryDate(expiryDate)) {
-  //     return res.status(400).json({ message: "Invalid expiry date" });
-  //   }
+  if (!isValidExpiryDate(expiryDate)) {
+    return res.status(400).json({ message: "Invalid expiry date" });
+  }
 
   try {
     const bankCard = await prisma.bankCard.create({
