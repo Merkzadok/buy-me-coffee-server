7c879b4c0b567982918d16a57ddbfe6df87ac79a
25LP1749 25LP1749 <merkzadok@gmail.com>
2025-08-06 10:07:18 +0800

    a

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import userRouter from "./router/user.router";
 import donationRouter from "./router/donation.router";
 import profileRouter from "./router/profile.router";
 import bankCardRouter from "./router/bank.router";
+import cors from "cors";
 
 const port = 4200;
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
