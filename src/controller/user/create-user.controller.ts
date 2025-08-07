1d0fbc7ace52440e3ec5b633bd637b5e87907ed5
25LP1749 25LP1749 <merkzadok@gmail.com>
2025-08-07 11:15:39 +0800

    sign-in

--- src/controller/user/create-user.controller.ts
+++ src/controller/user/create-user.controller.ts
@@ -45,11 +45,11 @@ export const createUser = async (req: Request, res: Response) => {
     if (await isMatch) {
       const data = { userId: user.id, email: user.email };
 
-      const secret = "super-secret-123456";
+      const secret = process.env.NEXT_PUBLIC_URL_JWT_SECRET;
 
       const hour = Math.floor(Date.now() / 1000) + 60 * 60;
 
-      const accessToken = jwt.sign({ exp: hour, data }, secret);
+      const accessToken = jwt.sign({ exp: hour, data }, secret as string);
 
       return res.status(200).json({ success: true, accessToken });
     } else {
@@ -57,5 +57,6 @@ export const createUser = async (req: Request, res: Response) => {
     }
   } catch (error) {
     res.status(500).json({ message: error });
+    console.log(error);
   }
 };
--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -5,6 +5,7 @@ import { getUser } from "../controller/user/get-users.controller";
 import { deleteUser } from "../controller/user/delete-user.controller";
 import { UpdateUser } from "../controller/user/update-user.controller";
 import { checkUserName } from "../controller/user/check-user-name-available.controller";
+import { signIn } from "../controller/user/sign-in.controller";
 
 const userRouter = express.Router();
 
@@ -19,5 +20,7 @@ userRouter.delete("/:userId", deleteUser);
 userRouter.put("/:userId", UpdateUser);
 
 userRouter.post("/check-username", checkUserName);
+
+userRouter.post("/sign-in", signIn);
 
 export default userRouter;
